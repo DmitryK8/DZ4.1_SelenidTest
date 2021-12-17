4174169e9c1824d795a3829ef61c29b05a6ec119
Dmitry Kravchenko <d.k.12.1972@gmail.com>
2021-12-17 22:02:57 +0500

    Готовая работа

--- src/test/java/ru/netology/testweb/SelenidTest.java
+++ src/test/java/ru/netology/testweb/SelenidTest.java
@@ -1,6 +1,5 @@
 package ru.netology.testweb;
 
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -16,12 +15,11 @@ public class SelenidTest {
     @Test
     void shouldTest() {
         open("http://localhost:9999");
-        SelenideElement form = $("form");
-        form.$("[data-test-id='city'] input").setValue("Москва");
-        form.$("[placeholder='Дата встречи']").setValue("20.12.2021");
-        form.$("[data-test-id='name'] input").setValue("Каренина-Подпоездом Анна");
-        form.$("[data-test-id='phone'] input").setValue("+79220000222");
-        form.$("[data-test-id='agreement']").click();
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[placeholder='Дата встречи']").setValue("20.12.2021");
+        $("[data-test-id='name'] input").setValue("Каренина-Подпоездом Анна");
+        $("[data-test-id='phone'] input").setValue("+79220000222");
+        $("[data-test-id='agreement']").click();
         $$(".button__text").find(exactText("Забронировать")).click();
         System.out.println("1");
         $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
