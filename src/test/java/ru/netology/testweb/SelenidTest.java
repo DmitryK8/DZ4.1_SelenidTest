f735eee0c890a7b8bce8e1ab7099a43cb37cd91b
Dmitry Kravchenko <d.k.12.1972@gmail.com>
2021-12-19 16:43:04 +0500

    переделал, нерабочий тест

--- src/test/java/ru/netology/testweb/SelenidTest.java
+++ src/test/java/ru/netology/testweb/SelenidTest.java
@@ -1,8 +1,11 @@
 package ru.netology.testweb;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
@@ -11,17 +14,25 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class SelenidTest {
 
+    String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    String planningDate = generateDate(4);
 
     @Test
     void shouldTest() {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[placeholder='Дата встречи']").setValue("20.12.2021");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Каренина-Подпоездом Анна");
         $("[data-test-id='phone'] input").setValue("+79220000222");
         $("[data-test-id='agreement']").click();
         $$(".button__text").find(exactText("Забронировать")).click();
         System.out.println("1");
         $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
     }
 }
