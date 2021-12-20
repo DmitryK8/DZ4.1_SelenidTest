b2e95f96923e15929647ca735f16ad50b88a81fc
Dmitry Kravchenko <d.k.12.1972@gmail.com>
2021-12-20 17:38:34 +0500

    пробный запуск

--- src/test/java/ru/netology/testweb/CardTest.java
+++ src/test/java/ru/netology/testweb/CardTest.java
@@ -1,38 +1,33 @@
 package ru.netology.testweb;
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.Keys.BACK_SPACE;
 
-public class SelenidTest {
+public class CardTest {
 
-    String generateDate(int days) {
+    public static String generateDate(int days) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
-    String planningDate = generateDate(4);
 
     @Test
     void shouldTest() {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").doubleClick().sendKeys(BACK_SPACE);
+        String planningDate = generateDate(3);
         $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Каренина-Подпоездом Анна");
         $("[data-test-id='phone'] input").setValue("+79220000222");
         $("[data-test-id='agreement']").click();
         $$(".button__text").find(exactText("Забронировать")).click();
-        System.out.println("1");
-        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
-        $(".notification__content").shouldBe(visible)
-                .shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
+        $(".notification__content").shouldHave(exactText("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
     }
 }
